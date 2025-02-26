2d28f9f59a155d74c9eb4cc55f0e0b26218ccf86
nichels02 <130480483+nichels02@users.noreply.github.com>
2025-02-26 16:54:21 -0500

    agregadoDeLasPaginasEnApp

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import './App.css';
 
 
 import Home from "./pages/Home.tsx";
+import Conocenos from "./pages/Conocenos.tsx";
+import Lidermania from "./pages/Lidermania.tsx";
+import Seguridad from "./pages/Seguridad.tsx";
+import Servicio from "./pages/Servicio.tsx";
+import Tecnologia from "./pages/Tecnologia.tsx";
 
 import BarraDeOpciones from './common/Componentes/BarraDeOpciones.tsx';
 import BarraDeOpciones2 from './common/Componentes/BarraDeOpciones2.tsx';
@@ -22,6 +27,11 @@ function App() {
                 <BarraDeOpcionesRedes />
                 <Routes>
                     <Route path='/' element={<Home/>}/>
+                    <Route path='/Conocenos' element={<Conocenos/>}/>
+                    <Route path='/Lidermania' element={<Lidermania/>}/>
+                    <Route path='/Seguridad' element={<Seguridad/>}/>
+                    <Route path='/Servicio' element={<Servicio/>}/>
+                    <Route path='/Tecnologia' element={<Tecnologia/>}/>
                 </Routes>
                 <Footer />
 
---
+++ src/pages/Conocenos.tsx
@@ -0,0 +1,50 @@
+import Header1 from '../common/Componentes/Header1.tsx';
+import EspacioVacio from '../common/Componentes/EspacioVacio.tsx';
+import Marquee from '../common/Componentes/Marquee.tsx';
+import CardWithExpand from '../common/Componentes/CardWithExpand.tsx';
+import FlechaGiro from '../common/Componentes/FlechaGiro.tsx';
+import PorcentajeNegocio from '../common/Componentes/PorcentajeNegocio.tsx';
+import DonaRotativa2 from '../common/Componentes/DonaRotativa2.tsx';
+import TextoTituloEImagen from '../common/Componentes/TextoTituloEImagen.tsx';
+import ContenedorScroll from '../common/Componentes/ContenedorScroll.tsx';
+import FormularioDeContacto from '../common/Componentes/FormularioDeContacto.tsx';
+
+
+
+
+
+function Conocenos(){
+
+    const imagenes = [
+        "https://wallpapers.com/images/hd/1920x1080-hd-space-u95406v61bxyrx3s.jpg",
+        "https://wallpapers.com/images/hd/1920x1080-aesthetic-glrfk0ntspz3tvxg.jpg",
+        "https://wallpapers.com/images/hd/1920-x-1080-hd-1qq8r4pnn8cmcew4.jpg",
+    ];
+
+    const textosBotones = ["Imagen 1", "Imagen 2", "Imagen 3"];
+    return (
+        <>
+            <Header1 />
+            <TextoTituloEImagen
+                titulo="Título de ejemplo"
+                texto="Este es un párrafo de ejemplo que acompaña al título. Este es un párrafo de ejemplo que acompaña al título. Este es un párrafo de ejemplo que acompaña al título. Este es un párrafo de ejemplo que acompaña al título. Este es un párrafo de ejemplo que acompaña al título. Este es un párrafo de ejemplo que acompaña al título."
+                imagenes={imagenes}
+                textosBotones={textosBotones}
+                imagenALaIzquierda={false}
+            />
+            <ContenedorScroll />
+            <FormularioDeContacto />
+            <Marquee />
+            <CardWithExpand />
+            <FlechaGiro />
+            <PorcentajeNegocio />
+            <DonaRotativa2 />
+
+
+
+            <EspacioVacio />
+
+        </>
+    )
+}
+export default Conocenos
