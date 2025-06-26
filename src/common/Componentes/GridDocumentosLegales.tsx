8539a4c179ca1ab84b07fbba4185a2c3c548d00b
Nichels <nicolas.derivero@tecsup.edu.pe>
2025-06-26 16:15:04 -0500

    GridDocumentosLegales(AgregadoDeTodosLosDocumentos)

--- src/common/Componentes/GridDocumentosLegales.tsx
+++ src/common/Componentes/GridDocumentosLegales.tsx
@@ -1,4 +1,4 @@
-// import { useState, useEffect } from "react";
+import { useState } from "react";
 // import { useContent } from './Sistemas/useContent';
 // import { useLanguage } from './Sistemas/LanguageContext';
 import styles from "../css/GridDocumentosLegales.module.css";
@@ -10,11 +10,44 @@ import { useContent } from './Sistemas/useContent';
 
 function GridDocumentosLegales() {
 
+    type SeccionTipo = "Politicas" | "Reglamentos" | "Procedimientos";
+    const secciones: SeccionTipo[] = ["Politicas", "Reglamentos", "Procedimientos"];
+
+
     const { language } = useLanguage();
-    const Documentos = useContent()?.PaginaLegal?.GridDocumentoLegal;
+
+    const [indiceSeleccionado, setIndiceSeleccionado] = useState(0);
+    const seccionActiva: SeccionTipo = secciones[indiceSeleccionado];
+
+
+
+    const Documentos = useContent()?.PaginaLegal?.GridDocumentoLegal?.[seccionActiva];
+
+
+
+
+
     return (
         <div className={styles.ContenedorPadre}>
             <div className={styles.contenedorCentrado}>
+                <div className={styles.ContenedorSecciones}>
+                    <button
+                        className={`${styles.Seccion} ${indiceSeleccionado === 0 ? styles.active : ''}`}
+                        onClick={() => setIndiceSeleccionado(0)}>
+                        {useContent()?.PaginaLegal?.GridDocumentoLegal?.Secciones[language].Politicas}
+                    </button>
+                    <button
+                        className={`${styles.Seccion} ${indiceSeleccionado === 1 ? styles.active : ''}`}
+                        onClick={() => setIndiceSeleccionado(1)}>
+                        {useContent()?.PaginaLegal?.GridDocumentoLegal?.Secciones[language].Reglamentos}
+                    </button>
+                    <button
+                        className={`${styles.Seccion} ${indiceSeleccionado === 2 ? styles.active : ''}`}
+                        onClick={() => setIndiceSeleccionado(2)}>
+                        {useContent()?.PaginaLegal?.GridDocumentoLegal?.Secciones[language].Procedimientos}
+                    </button>
+                </div>
+                <div className={styles.Separador}/>
                 <div className={styles.GridPadre}>
 
                     {Documentos?.map((doc, index) => (
