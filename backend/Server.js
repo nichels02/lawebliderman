c14f6c304390f27b9e9a089398addeff3815773f
Nicolas <nicolas.derivero@tecsup.edu.pe>
2025-12-17 11:10:35 -0500

    Backend

--- backend/Server.js
+++ backend/Server.js
@@ -22,14 +22,14 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-
+{/*
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
-
+*/}
 {/*db.connect(err => {
     if (err) {
         console.error('Error conectando a MySQL:', err);
