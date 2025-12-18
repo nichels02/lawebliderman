a98b3087b2071cd06e0ddd9d525699d6d3579bb2
Nicolas <nicolas.derivero@tecsup.edu.pe>
2025-12-18 11:36:48 -0500

    Backend

--- backend/EnviarCorreo-2.0.js
+++ backend/EnviarCorreo-2.0.js
@@ -1,41 +1,22 @@
-const SibApiV3Sdk = require('sib-api-v3-sdk');
+import SibApiV3Sdk from 'sib-api-v3-sdk';
 
-// Configuración del cliente
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const sendEmail = async ({ to, subject, html }) => {
+    const client = SibApiV3Sdk.ApiClient.instance;
+    client.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
 
-// API Key
-const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.BREVO_API_KEY; // ponla en tu .env
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-// Crear instancia de la API de emails
-const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-
-// Datos del correo
-const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail({
-    subject: 'Correo de prueba desde Node',
-    htmlContent: `
-    <h2>Hola 👋</h2>
-    <p>Este correo fue enviado usando <b>Brevo</b> desde Node.js</p>
-  `,
-    sender: {
-        name: 'Mi Web',
-        email: 'nderiveropuga9802@gmail.com', // DEBE estar verificado en Brevo
-    },
-    to: [
-        {
-            email: 'nderiveropuga9802@gmail.com',
-            name: 'Usuario',
+    const email = {
+        subject,
+        htmlContent: html,
+        sender: {
+            name: 'La Web Liderman',
+            email: 'nderiveropuga9802@gmail.com', // debe estar verificado
         },
-    ],
-});
+        to: [{ email: 'nderiveropuga9802@gmail.com' }],
+    };
 
-async function sendEmail() {
-    try {
-        const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
-        console.log('Correo enviado:', response.messageId);
-    } catch (error) {
-        console.error('Error enviando correo:', error);
-    }
-}
+    return apiInstance.sendTransacEmail(email);
+};
 
 export default sendEmail;
